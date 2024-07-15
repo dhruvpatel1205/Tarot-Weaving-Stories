b8ab798a8a0459d9e5aac20197ecf970dfa01c5a
Dhruv Patel <dhruvj1205@gmail.com>
2024-07-15 14:01:24 -0700

    Code change

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,3 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 from webapp import connect
+
+app.run(debug=True)
--- webapp/main.py
+++
@@ -1,5 +0,0 @@
-# Run this main to Compile & Run whole application
-
-from webapp import app
-
-app.run(debug=True)
